30014c9b06fae7d420e264332e93df42f276fa0d
Gary Homewood <gary.homewood@gmail.com>
2015-09-02 17:05:52 +0100

    implementing talkback

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,23 @@ el.addEventListener('click', function() {
         var utterance = new SpeechSynthesisUtterance();
         utterance.voice = 'Google UK English Male';
         utterance.lang = 'en-GB';
-        utterance.text = 'Yesterday I resolved';
+        var issues = document.getElementsByClassName('talkback-content');
+
+        if (issues.length > 0) {
+        talkback = "";
+        for (var i = 0; i < issues.length; i++) {
+            if (i == 0) {
+                talkback =  'Yesterday I resolved ';
+            } else {
+                talkback += " and ";
+            }
+            talkback += issues[i].innerHTML + ".";
+        }
+
+        talkback += " and I have no blockers";
+
+        utterance.text = talkback;
         window.speechSynthesis.speak(utterance);
+        }
     }
 }, false);
